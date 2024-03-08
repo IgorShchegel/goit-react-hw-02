90459bfd474c72c34bb7f3047e6f43a5ea78e652
IgorShchegel <mitosss777@gmail.com>
2024-03-08 19:34:55 +0200

    hw2

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import Notification from './components/Feedback/Notification/Notification';
 import './App.css'
 
 function App() {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("feedback")) ?? {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+  );
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   const positivePercentage = Math.round(((feedback.good + feedback.neutral) / totalFeedback) * 100);
    
-   
-  useEffect(() => {
-    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
-    if (savedFeedback) {
-      setFeedback(savedFeedback);
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(feedback));
@@ -34,7 +34,6 @@ function App() {
   const resetFeedback = () => {
     const newFeedback = { good: 0, neutral: 0, bad: 0 };
     setFeedback(newFeedback);
-    localStorage.setItem('feedback', JSON.stringify(newFeedback));
   };
 
   return (
